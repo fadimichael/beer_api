a09ffceaaaa5f4c753ea9f825f73b88a2b103302
fadimichael <fadi.michael@gmx.de>
2022-12-02 11:54:49 +0100

    letzte update

--- src/components/BeerItems.js
+++ src/components/BeerItems.js
@@ -1,5 +1,17 @@
-const BeerItems = () => {
-  return <div></div>;
+import { Link } from "react-router-dom";
+const BeerItems = (props) => {
+  return (
+    <article className="grid-item">
+      <div className="left">
+        <img src={props.image_url} alt="" />
+      </div>
+      <div className="right">
+        <h2>{props.name}</h2>
+        <h3>{props.tagline}</h3>
+        <p>Created by: {props.name}</p>
+      </div>
+    </article>
+  );
 };
 
 export default BeerItems;
--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,5 +1,27 @@
+import React, { useState, useEffect } from "react";
+import BeerItems from "../components/BeerItems";
+
 const Overview = () => {
-  return <div></div>;
+  const [beers, setBeers] = useState([]);
+  const showBeers = () => {
+    fetch("https://ih-beers-api2.herokuapp.com/beers")
+      .then((res) => res.json())
+      .then((json) => setBeers(json));
+  };
+  useEffect(showBeers, []);
+  console.log(beers);
+  return (
+    <section className="grid">
+      {beers.map((beer) => (
+        <BeerItems
+          key={beer.id}
+          image_url={beer.image_url}
+          name={beer.name}
+          tagline={beer.tagline}
+        />
+      ))}
+    </section>
+  );
 };
 
 export default Overview;

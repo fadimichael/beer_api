79a29b5a407631a4090d5ff96188e872f1d739ab
fadimichael <fadi.michael@gmx.de>
2022-12-02 15:08:36 +0100

    letzte update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,28 @@
 import all_beers from "../data/all_beers.png";
 import random_beer from "../data/random_beer.png";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Link to="/beer">
         <img src={all_beers} alt="" />
       </Link>
       <h2>All Beers</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat laborum
-        fuga facere non similique tempore?
+        fuga facere non similique tempore? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Fugiat laborum fuga facere non similique
+        tempore?
       </p>
       <img src={random_beer} alt="" />
       <h2>All Beers</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat laborum
-        fuga facere non similique tempore?
+        fuga facere non similique tempore? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Fugiat laborum fuga facere non similique
+        tempore?
       </p>
     </div>
   );

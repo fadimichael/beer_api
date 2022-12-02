bd88ae7500261132d5bd9368bc63aeb1b0079443
YubiFuu <jess.supercode@gmail.com>
2022-12-02 13:49:13 +0100

    overview/footer fetch

--- src/components/FooterBtn.js
+++ src/components/FooterBtn.js
@@ -1,16 +1,17 @@
 import logo from "../data/Logo.png";
 import { Link } from "react-router-dom";
+import "./FooterBtn.css";
 
 const FooterBtn = () => {
-  return (
-    <footer>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-      </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div>
+                <Link to="/">
+                    <img src={logo} alt="logo" />
+                </Link>
+            </div>
+        </footer>
+    );
 };
 
 export default FooterBtn;

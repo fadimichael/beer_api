8f0f975f8129146ac44bad8e21662b82b4dc94e1
fadimichael <fadi.michael@gmx.de>
2022-12-02 10:39:29 +0100

    structure

---
+++ src/components/BeerDetails.js
@@ -0,0 +1,5 @@
+const BeerDetails = () => {
+  return <div></div>;
+};
+
+export default BeerDetails;
---
+++ src/components/BeerItems.js
@@ -0,0 +1,5 @@
+const BeerItems = () => {
+  return <div></div>;
+};
+
+export default BeerItems;
